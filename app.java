4dba5e90692fe00002f9c6cbb5a2d04d6c63243c
root <root@ip-172-31-20-242.ec2.internal>
2025-11-06 09:30:44 +0000

    modified code

--- app.java
+++ app.java
@@ -1,6 +1,12 @@
+
 public class HelloWorld {
     public static void main(String[] args) {
         // This line prints "Hello, World!" to the console
-        System.out.println("Hello, World!"); 
-    }
+        System.out.println("Hello, World!
+
+new code by deveoper 1 without pulling code-
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
 }
