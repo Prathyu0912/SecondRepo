60997c0bf1bbd105fbf4e0410fe153d396dacce3
Prathyu0912 <lakshmiprathyusha1209@gmail.com>
2025-11-06 15:03:42 +0530

    added another line in app.java

--- app.java
+++ app.java
@@ -9,6 +9,6 @@ public class HelloWorld {
 new code by deveoper 1 without pulling code-
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    System.out.println("Prathyusha");
   }
 }
