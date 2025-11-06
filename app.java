10da61d9dcd31eb86dbdab70ed1bce05403c266b
Prathyu0912 <lakshmiprathyusha1209@gmail.com>
2025-11-06 14:48:22 +0530

    added fresh code in app.java

--- app.java
+++ app.java
@@ -1,2 +1,6 @@
-This is java file adding some thing
---------------------------------
+public class HelloWorld {
+    public static void main(String[] args) {
+        // This line prints "Hello, World!" to the console
+        System.out.println("Hello, World!"); 
+    }
+}
