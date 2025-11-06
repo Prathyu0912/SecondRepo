67930386f17ab38f920094887918a72b1e467724
Prathyu0912 <lakshmiprathyusha1209@gmail.com>
2025-11-06 15:01:57 +0530

    commited again app.java

--- app.java
+++ app.java
@@ -3,6 +3,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         // This line prints "Hello, World!" to the console
         System.out.println("Hello, World!
+                           }
+}
 
 new code by deveoper 1 without pulling code-
 public class Main {
